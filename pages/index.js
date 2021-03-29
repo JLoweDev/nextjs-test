891560fb301f8e9f83e1a568d9433d29213e589d
James <67014970+JLoweDev@users.noreply.github.com>
2021-03-29 20:45:31 +0100

    brought in server to determine root http

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import {server} from '../config/index'
 import Head from 'next/head'
 import ArticleList from '../components/ArticleList'
 
@@ -15,7 +16,7 @@ export default function Home({articles}) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(``)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
 
   return {
